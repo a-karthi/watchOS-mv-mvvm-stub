d77f1f660dd94b09aac1bb731f0698d0435ec45e
a-karthi <92154198+a-karthi@users.noreply.github.com>
2023-03-27 12:51:49 +0530

    Add files via upload

---
+++ SampleWatch Watch App/SampleWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleWatchApp.swift
+//  SampleWatch Watch App
+//
+//  Created by @karthi on 27/03/23.
+//
+
+import SwiftUI
+
+@main
+struct SampleWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SampleWatch/SampleWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleWatchApp.swift
+//  SampleWatch
+//
+//  Created by @karthi on 27/03/23.
+//
+
+import SwiftUI
+
+@main
+struct SampleWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
